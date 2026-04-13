3ba6629d49297f34dee60ac3d7ae5b312fbb9e19
monishahs6 <monishahs784@gmail.com>
2026-04-13 08:36:24 +0530

    Add files via upload

---
+++ 2d finding highest among 3 numbers.py
@@ -0,0 +1,9 @@
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+if a>b and a>c:
+    print("a is a highest");
+    elif b>c:
+        else;
+        print("c is a highest")
+        
