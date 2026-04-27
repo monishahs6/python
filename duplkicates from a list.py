2ac54e5ab835a5cdcc05782ab50a3cdfd5b8b2e5
monishahs6 <monishahs784@gmail.com>
2026-04-27 08:33:35 +0530

    Add files via upload

---
+++ duplkicates from a list.py
@@ -0,0 +1,4 @@
+lst = [1, 2, 2, 3, 4, 4, 5]
+
+unique_list = list(set(lst))
+print("List after removing duplicates:", unique_list)
---
+++ intersection of two sets.py
@@ -0,0 +1,4 @@
+set1 = {1, 2, 3}
+set2 = {2, 3, 4}
+intersection_set = set1 & set2
+print("Intersection:", intersection_set)
